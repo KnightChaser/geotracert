c0fb94e20bf73b920632ed3f17d28f9ba5272b3a
Lee Garam <agerio100@naver.com>
2024-05-04 03:13:29 +0900

    More information button
    Additional detailed information will be shown up via clickable button, based on Sweetalert2 and Bootstrap(a little).

--- static/index.js
+++ static/index.js
@@ -14,6 +14,7 @@ async function fetchTraceroute() {
         const cellLocation = row.insertCell();
         const cellCountry = row.insertCell();
         const cellOrganization = row.insertCell();
+        const cellMoreInfo = row.insertCell();
 
         // Handle null values and rounding of RTT
         cellHop.textContent = ipInfo.hop || "-";
@@ -27,7 +28,66 @@ async function fetchTraceroute() {
         if (ipInfo.ip_details && ipInfo.ip_details.countryCode)
             cellCountry.innerHTML = getFlagEmoji(ipInfo.ip_details.countryCode) + " " + cellCountry.innerHTML;
         cellOrganization.textContent = ipInfo.ip_details ? ipInfo.ip_details.org : "-";
-    });
+
+        // Add a button to show more information
+        // IP, Continent, country, region, regionName, city, district, zip, timezone, reverse 
+        const moreInfoButton = document.createElement("button");
+        moreInfoButton.setAttribute("class", "btn btn-primary");
+        moreInfoButton.textContent = "More Info";
+        cellMoreInfo.appendChild(moreInfoButton);
+        moreInfoButton.onclick = () => {
+        Swal.fire({
+            title: "More Information",
+            html: `
+                <table class='table'>
+                    <thead>
+                        <tr>
+                            <th>Key</th>
+                            <th>Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>IP Address</td>
+                            <td><code>${ipInfo.ip}</code></td>
+                        </tr>
+                        <tr>
+                            <td>Continent</td>
+                            <td>${ipInfo.ip_details.continent || "-"}</td>
+                        </tr>
+                        <tr>
+                            <td>Country</td>
+                            <td>${ipInfo.ip_details.country || "-"}</td>
+                        </tr>
+                        <tr>
+                            <td>Region</td>
+                            <td>${ipInfo.ip_details.region || "-"} (${ipInfo.ip_details.regionName || "-"})</td>
+                        </tr>
+                        <tr>
+                            <td>City</td>
+                            <td>${ipInfo.ip_details.city || "-"}</td>
+                        </tr>
+                        <tr>
+                            <td>District</td>
+                            <td>${ipInfo.ip_details.district || "-"}</td>
+                        </tr>
+                        <tr>
+                            <td>ZIP</td>
+                            <td>${ipInfo.ip_details.zip || "-"}</td>
+                        </tr>
+                        <tr>
+                            <td>Timezone</td>
+                            <td>${ipInfo.ip_details.timezone || "-"}</td>
+                        </tr>
+                        <tr>
+                            <td>Reverse DNS</td>
+                            <td>${ipInfo.ip_details.reverse || "-"}</td>
+                        </tr>
+                    </tbody>
+                </table>`,
+            confirmButtonText: "Close"
+        });
+    }});
 }
 
 // Get the flag emoji for a given country code
