8ef6b5e83d0723acf65817eec25bde7e4f7c3c86
Lee Garam <agerio100@naver.com>
2024-05-06 16:55:54 +0900

    Substitute more info button icon into search icon
    More simplicity and visual intuitiveness

--- static/index.js
+++ static/index.js
@@ -184,7 +184,8 @@ function appendHopInfo(row, ipInfo) {
     // Add a button to display more detailed information
     const moreInfoButton = document.createElement("button");
     moreInfoButton.className = "btn btn-primary";
-    moreInfoButton.textContent = "More Info";
+    // Search icon from Bootstrap Icons
+    moreInfoButton.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search' viewBox='0 0 16 16'><path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0'/></svg>" 
     moreInfoButton.onclick = () => showMoreInfo(ipInfo);
     cellMoreInfo.appendChild(moreInfoButton);
 }
